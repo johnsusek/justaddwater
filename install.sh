4fc00710eb115558c16e600dbfd9640bd9bf8bc4
John Susek <john@johnsolo.net>
2017-12-25 11:06:13 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get upgrade
+# reboot
+apt-get install postgresql postgresql-contrib
+
+# Download and run postgrest
+wget https://github.com/begriffs/postgrest/releases/download/v0.4.3.0/postgrest-v0.4.3.0-ubuntu.tar.xz
+tar Jxf postgrest-v0.4.3.0-ubuntu.tar.xz
+./postgrest ./postgrest.conf
