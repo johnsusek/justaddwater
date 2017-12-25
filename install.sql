7e265bc06c79455fa99f9c7ef63f06fe9f6848a2
John Susek <john@johnsolo.net>
2017-12-25 11:22:32 -0600

    Update install.sql

--- install.sql
+++ install.sql
@@ -12,18 +12,3 @@ create role web_authenticated nologin;
 grant web_authenticated to postgres;
 grant usage on schema api to web_authenticated;
 grant usage, select on all sequences in schema api to web_authenticated;
-
--- example API object: "log"
-create table api.log (
-  id serial primary key,
-  namespace text not null,
-  message jsonb not null,
-  visitor jsonb 
-);
-
-grant all on api.log to web_authenticated;
--- typically we would grant select on anonymous requests
--- but since this is a log collector example, we grant only insert
--- so users cannot see all the log entries
-grant insert on api.log to web_anon;
-grant all on api.log to postgres;
