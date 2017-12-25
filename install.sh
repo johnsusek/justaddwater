79f19d3247315729dc137e0e535669e66b1b4119
John Susek <john@johnsolo.net>
2017-12-25 12:06:47 -0600

    nginx config

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 apt-get update
 apt-get upgrade
-apt-get install postgresql postgresql-contrib
+apt-get install nginx postgresql postgresql-contrib
+
+# nginx
+mv nginx.conf /etc/nginx/
 
 # Download and run postgrest
 wget https://github.com/begriffs/postgrest/releases/download/v0.4.3.0/postgrest-v0.4.3.0-ubuntu.tar.xz
