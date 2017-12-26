14101b06236c000b5fc78ebeef32cdf3d4769e79
John Susek <john@johnsolo.net>
2017-12-26 17:02:22 -0600

    no message

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-apt-get -Y update
-apt-get -Y upgrade
-apt-get -Y install nginx postgresql postgresql-contrib 
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install nginx postgresql postgresql-contrib 
 
 mv nginx.conf /etc/nginx/
 service nginx restart
 
-export POSTGREST_VERSION = "0.4.3.0"
+export POSTGREST_VERSION="0.4.3.0"
 wget https://github.com/begriffs/postgrest/releases/download/v$POSTGREST_VERSION/postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz
 tar Jxf postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz
