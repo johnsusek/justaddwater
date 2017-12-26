da08664363ba8b857678ce67f12cf381cab6a12e
John Susek <john@johnsolo.net>
2017-12-26 17:05:42 -0600

    no message

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ mv nginx.conf /etc/nginx/
 service nginx restart
 
 export POSTGREST_VERSION="0.4.3.0"
-wget https://github.com/begriffs/postgrest/releases/download/v$POSTGREST_VERSION/postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz
+wget -nv https://github.com/begriffs/postgrest/releases/download/v$POSTGREST_VERSION/postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz 
 tar Jxf postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz
