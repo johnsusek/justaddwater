e4e0a7458661fdd4233545c8d96c87c93e7061f9
John Susek <john@johnsolo.net>
2017-12-27 12:14:23 -0600

    Server stuff

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 echo "* Downloading nginx & postgresql packages..."
 apt-get -y update >/dev/null
 apt-get -y upgrade >/dev/null
-apt-get -y install nginx postgresql postgresql-contrib >/dev/null
+apt-get -y install nginx postgresql postgresql-contrib postgresql-server-dev-all build-essential >/dev/null
+echo "shared_preload_libraries='safeupdate'" >> /etc/postgresql/9.5/main/postgresql.conf;
+service postgresql restart
 
 echo "* Installing nginx config"
 [ -e nginx.conf ] && mv nginx.conf /etc/nginx/
@@ -12,5 +14,11 @@ POSTGREST_VERSION="0.4.3.0"
 wget -q https://github.com/begriffs/postgrest/releases/download/v$POSTGREST_VERSION/postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz 
 tar Jxf postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz
 rm postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz*
+
+echo "* Configuring firewall for 22,80,443";
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow proto tcp from any to any port 22,80,443
+ufw --force enable
 
 echo "Installation complete. Don't forget to edit postgrest.conf. Then run postgrest.sh."
