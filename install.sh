7d0146055f06984244015fd4ff22fe6e76cef73f
John Susek <john@johnsolo.net>
2017-12-26 18:07:56 -0600

    no message

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ echo "* Installing nginx config"
 service nginx restart
 
 echo "* Downloading PostgREST..."
-export POSTGREST_VERSION="0.4.3.0"
+POSTGREST_VERSION="0.4.3.0"
 wget -q https://github.com/begriffs/postgrest/releases/download/v$POSTGREST_VERSION/postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz 
 tar Jxf postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz
 rm postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz*
 
-echo "Installation complete. Don't forget to edit postgrest.conf. Then run `postgrest.sh` and check :3000."
+IP=ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'
+echo "Installation complete. Don't forget to edit postgrest.conf. Then run postgrest.sh and check http://$IP:3000."
