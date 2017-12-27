386a062a78c33fc8d20bf1de957ccefbf58f6bd0
John Susek <john@johnsolo.net>
2017-12-27 11:08:00 -0600

    no message

--- example_schema.sql
+++ example_schema.sql
@@ -1,9 +1,11 @@
--- This is where you step in and start describing your data structure, then watch the api get generated around it.
+-- This is where you step in and start describing your data 
+-- structure, then watch the api get generated around it.
 
--- In this example, we have a log collecting api. Anonymous users will send in log messages to our api.
+-- In this example, we have a log collecting api. Anonymous users
+--  will send in log messages to our api.
 
--- example API object: "logentry"
-create table api.logentry (
+-- example API object: "log entries"
+create table api.entries (
   id serial primary key,
   namespace text not null,
   message jsonb not null,
@@ -11,13 +13,16 @@ create table api.logentry (
 );
 
 -- typically on anonymous requests we would grant select only
--- but since this is a log collector example, we grant only insert
--- this will give anonymous web users access to a POST endpoint on /log
-grant select,insert on api.logentry to web_anon;
+-- but since this is a log collector example, we grant insert too
+revoke all on api.entries from web_anon;
+grant select,insert on api.entries to web_anon;
 
--- If you have a JWT token set up (see docs), there will be a web_authenticated role for auth'd users
-grant all on api.log to web_authenticated;
+-- If you have a JWT token set up (see docs), there will be a 
+-- web_authenticated role for auth'd users
+revoke all on api.entries from web_authenticated;
+grant all on api.entries to web_authenticated;
 
 -- we always give the postgres user permissions for internal use
-grant all on api.logentry to postgres;
+grant all on api.entries to postgres;
+
 
