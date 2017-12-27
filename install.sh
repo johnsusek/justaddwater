149b6e646e9a2d490f49776a9c4fccddf7b7fb39
John Susek <john@johnsolo.net>
2017-12-26 18:05:10 -0600

    no message

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
-echo "Downloading Prerequisites..."
+echo "* Downloading nginx & postgresql packages..."
 apt-get -y update >/dev/null
 apt-get -y upgrade >/dev/null
 apt-get -y install nginx postgresql postgresql-contrib >/dev/null
 
-mv nginx.conf /etc/nginx/ >/dev/null 2>&1
+echo "* Installing nginx config"
+[ -e nginx.conf ] && mv nginx.conf /etc/nginx/
 service nginx restart
 
-echo "Downloading PostgREST..."
+echo "* Downloading PostgREST..."
 export POSTGREST_VERSION="0.4.3.0"
 wget -q https://github.com/begriffs/postgrest/releases/download/v$POSTGREST_VERSION/postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz 
 tar Jxf postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz
 rm postgrest-v$POSTGREST_VERSION-ubuntu.tar.xz*
 
-echo "Installation complete. Don't forget to edit postgrest.conf."
+echo "Installation complete. Don't forget to edit postgrest.conf. Then run `postgrest.sh` and check :3000."
--- install.sql
+++ install.sql
@@ -1,14 +1,18 @@
 -- database
+drop schema if exists api;
 create schema api;
 
 -- user - anonymous
+drop role if exists web_anon;
 create role web_anon nologin;
 grant web_anon to postgres;
 grant usage on schema api to web_anon;
 grant usage on all sequences in schema api TO web_anon;
 
 -- user - authenticated
+drop role if exists web_authenticated;
 create role web_authenticated nologin;
 grant web_authenticated to postgres;
 grant usage on schema api to web_authenticated;
 grant usage, select on all sequences in schema api to web_authenticated;
+
